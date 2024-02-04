783646fa0a562fa42be8e37b7a07d7e75f11d6d5
manne0081@gmail.com <manne0081@gmail.com>
2024-02-04 18:30:37 +0100

    header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
+        this.router.navigate(['private/dashboard']);
         this.router.navigate(['private/workspace']);
     }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
+
 export class HeaderComponent implements OnInit {
+    activeUrl: string = '';
+    ngClassItemSearching: string = "header-button-bg-1";
+    ngClassItemFavorites: string = "header-button-bg-1";
+    ngClassItemDashboard: string = "header-button-bg-1";
+    ngClassItemWorkspace: string = "header-button-bg-1";
+    ngClassItemContacts: string = "header-button-bg-1";
 
-  constructor() { }
+    constructor(private router: Router) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        // console.log("HeaderComponent > ngOnInit" + this.router.url);
+        this.setClassToItem(this.router.url);
+    }
 
+    setClassToItem(route: string): void {
+        console.log(route);
+
+        if (route.substring(0,18) == "/private/dashboard") {
+			this.ngClassItemFavorites = "header-button-bg-1 pre-active";
+			this.ngClassItemDashboard = "header-button-bg-1 active";
+			this.ngClassItemWorkspace = "header-button-bg-1 post-active";
+            this.ngClassItemContacts = "header-button-bg-1";
+		}
+        if (route.substring(0,18) == "/private/workspace") {
+            this.ngClassItemFavorites = "header-button-bg-1";
+			this.ngClassItemDashboard = "header-button-bg-1 pre-active";
+			this.ngClassItemWorkspace = "header-button-bg-1 active";
+			this.ngClassItemContacts = "header-button-bg-1 post-active";
+		}
+    }
 }
